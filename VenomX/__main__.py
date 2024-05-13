d6eab44fd327dd11add465544144a3bba3cc124e
mariyam840 <159921630+mariyam840@users.noreply.github.com>
2024-05-13 14:38:30 +0530

    Update __main__.py

--- VenomX/__main__.py
+++ VenomX/__main__.py
@@ -34,7 +34,7 @@ async def main():
     await run_async_inline()
     logs.info("Successfully Deployed !!")
     await run_async_calls()
-    logs.info("Do Visit - @VenomOwners")
+    logs.info("Do Visit - @Dil_Ka_Ehsaas")
     await idle()
 
 
